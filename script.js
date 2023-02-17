d04709c9ef20b21c4386b154fd3fa06dfcfeaa50
Simon Stein <spstein@willamette.edu>
2023-02-17 14:47:24 -0800

    Update script.js

--- script.js
+++ script.js
@@ -60,3 +60,17 @@ var swiper = new Swiper(".mySwiper", {
       currentNav.classList.add('nav-blue');
     }
   });
+
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if(entry.isIntersecting){
+        entry.target.classList.add('show');
+      } else{
+        entry.target.classList.remove('show');
+      }
+    });
+  });
+  
+  const hiddenElements = document.querySelectorAll('.hidden');
+  hiddenElements.forEach((el) => observer.observe(el));
